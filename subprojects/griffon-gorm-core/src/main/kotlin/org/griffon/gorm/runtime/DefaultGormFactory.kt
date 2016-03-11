69f44b5eb0902589b06601b4842654d1c587915c
Leon <sleonidy@gmail.com>
2016-03-11 16:38:26 +0200

    Update License headers in sources

--- subprojects/griffon-gorm-core/src/main/kotlin/org/griffon/gorm/runtime/DefaultGormFactory.kt
+++ subprojects/griffon-gorm-core/src/main/kotlin/org/griffon/gorm/runtime/DefaultGormFactory.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.griffon.gorm.runtime
 
 import griffon.core.Configuration
@@ -15,9 +30,7 @@ import javax.inject.Inject
 import javax.inject.Named
 import javax.sql.DataSource
 
-/**
- * Created by lyanovsk on 24-Feb-16.
- */
+
 
 abstract class DefaultGormFactory @Inject constructor(@Nonnull @Named("gorm") val configuration: Configuration, @Nonnull val application: GriffonApplication) :
         GormFactory {
--- subprojects/griffon-gorm-hibernate5/src/test/groovy/org/griffon/gorm/GormHibernate5Spec.groovy
+++ subprojects/griffon-gorm-hibernate5/src/test/groovy/org/griffon/gorm/GormHibernate5Spec.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.griffon.gorm
 
 import griffon.core.GriffonApplication
@@ -13,9 +28,7 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 import javax.inject.Inject
-/**
- * Created by lyanovsk on 24-Feb-16.
- */
+
 @Unroll
 class GormHibernate5Spec extends Specification {
     static {
